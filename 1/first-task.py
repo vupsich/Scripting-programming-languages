5adc0470df6d21063b38843eeb799a257a808cf5
Oleg Kudryashov <olegkudryashov.irk@gmail.com>
2024-09-15 13:49:23 +0800

    The first task is completed

---
+++ 1/first-task.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+r = requests.get('https://jsonplaceholder.typicode.com/posts/')
+data = r.json()
+mas = [i for i in data if i['userId'] % 2 == 0]
+print(json.dumps(mas, indent=4))
