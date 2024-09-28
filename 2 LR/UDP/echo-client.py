8dd8ff273a1d71e7ce17fdfffeb7c9dade668223
Oleg Kudryashov <olegkudryashov.irk@gmail.com>
2024-09-28 20:29:37 +0800

    LR 2 is done

--- 2 LR/UDP/echo-client.py
+++ 2 LR/UDP/echo-client.py
@@ -1,13 +1,12 @@
 import socket
 
-HOST = "127.0.0.1"  # IP-адрес сервера
-PORT = 65432  # Порт, используемый сервером
+HOST = "127.0.0.1" 
+PORT = 65432  
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    # Отправляем сообщение на сервер
     s.sendto(b"Hello", (HOST, PORT))
 
-    # Получаем ответ от сервера
-    data, server = s.recvfrom(1024)  # Максимальный размер сообщения 1024 байта
+    data, server = s.recvfrom(1024)  
+    print(f"Received {data!r} from {server}")
 
-print(f"Received {data!r} from {server}")
+    s.sendto(b"Exit", (HOST, PORT))
